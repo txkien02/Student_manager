de514e6008899fe33c95c03ee38a14242ebb8b6a
TxKien <81473046+txkien02@users.noreply.github.com>
2023-09-08 20:44:43 +0700

    update1 d

--- Web_Student_manager/Controllers/AdminController.cs
+++ Web_Student_manager/Controllers/AdminController.cs
@@ -1,20 +1,90 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Data.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Newtonsoft.Json;
 using System.Data;
+using System.Net.Http;
+using System.Text;
 using Web_Student_manager.Filters;
 
 namespace Web_Student_manager.Controllers
 {
+
     [AuthorFilterFilter("Admin")]
     public class AdminController : Controller
     {
         // GET: AdminController
-        public ActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public AdminController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7164/api/Admin"); // Thay đổi địa chỉ API thật của bạn
+        }
+        public async Task<ActionResult> Index()
+        {
+            var httpContext = context.HttpContext; // Lấy HttpContext từ AuthorizationFilterContext
+            var httpClient = _httpClientFactory.CreateClient();
+            if (httpContext.Session.TryGetValue("JWToken", out byte[] tokenBytes))
+            {
+                var jwtToken = Encoding.UTF8.GetString(tokenBytes);
+
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + jwtToken);
+
+                var response = await httpClient.GetAsync("https://localhost:7164/api/Authorization/GetRole");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var apiResponse = JsonConvert.DeserializeObject<Status>(jsonResponse);
+
+                    
+                }
+                else
+                {
+                    context.Result = new UnauthorizedResult(); // Không xác thực, trả về lỗi 401 Unauthorized
+                    context.HttpContext.Response.Redirect("/Login/Index"); // Chuyển hướng đến trang đăng nhập
+                    return;
+                }
+
+            }
+            else
+            {
+                context.Result = new UnauthorizedResult(); // Không xác thực, trả về lỗi 401 Unauthorized
+                context.HttpContext.Response.Redirect("/Login/Index"); // Chuyển hướng đến trang đăng nhập
+                return;
+            }
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(ClassModel newClass)
+        {
+            if (ModelState.IsValid)
+            {
+                // Thêm lớp học vào cơ sở dữ liệu
+                // Cập nhật tổng số học sinh sau khi thêm
+                // Redirect đến trang danh sách lớp học
+            }
+            return View(newClass);
+        }
+
+        // Action để sửa thông tin lớp học
+        [HttpPost]
+        public IActionResult Edit(ClassModel editedClass)
+        {
+            if (ModelState.IsValid)
+            {
+                // Cập nhật thông tin lớp học trong cơ sở dữ liệu
+                // Redirect đến trang danh sách lớp học
+            }
+            return View(editedClass);
+        }
+
         
+
+
     }
 }
--- Web_Student_manager/Program.cs
+++ Web_Student_manager/Program.cs
@@ -13,64 +13,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddMvc();
 builder.Services.AddHttpClient();
 
-// Cấu hình xác thực bằng JWT Token
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JWT:ValidAudience"],
-            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-            ClockSkew = TimeSpan.Zero,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-        };
-    }).AddCookie(options =>
-    {
-        options.LoginPath = "/Login/Index";
-    }); ;
 
 builder.Services.AddSession(option =>
 {
     option.IdleTimeout = TimeSpan.FromMinutes(20);
 
 });
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("Admin", policy =>
-    {
-        policy.RequireAuthenticatedUser(); // Yêu cầu người dùng đã xác thực
-
-        // Kiểm tra vai trò từ Session
-        policy.RequireAssertion(context =>
-        {
-            var userRole = context.User.FindFirstValue("UserRole"); // Lấy vai trò từ thông tin xác thực
-            if (!string.IsNullOrEmpty(userRole) && userRole == "Admin")
-            {
-                return true; // Người dùng có vai trò "Admin"
-            }
-            return false; // Người dùng không có vai trò "Admin"
-        });
-    });
-    options.AddPolicy("User", policy =>
-    {
-        policy.RequireAuthenticatedUser(); // Yêu cầu người dùng đã xác thực
-
-        // Kiểm tra vai trò từ Session
-        policy.RequireAssertion(context =>
-        {
-            var userRole = context.User.FindFirstValue("UserRole"); // Lấy vai trò từ thông tin xác thực
-            if (!string.IsNullOrEmpty(userRole) && (userRole == "User"))
-            {
-                return true; // Người dùng có vai trò "Admin" hoặc "User"
-            }
-            return false; // Người dùng không có vai trò "Admin" hoặc "User"
-        });
-    });
-});
-
-
 
 var app = builder.Build();
 
